8cfa05613fc18552deba7a2f0e80e4b87e30dbcd
Muhammad Danial <mrdanial526@example.com>
2026-07-27 22:58:19 +0500

    Fix Vercel serverless deployment

--- server/server.js
+++ server/server.js
@@ -21,12 +21,8 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use(notFound);
 app.use(errorHandler);
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
